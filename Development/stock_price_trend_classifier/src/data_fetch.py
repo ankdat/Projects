a28d2e339250a9ed17f1e4c104a462c64803865e
ankdat <ankan99datta@gmail.com>
2026-01-14 01:08:28 +0530

    Initial Commit

---
+++ Development/stock_price_trend_classifier/src/data_fetch.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import pandas as pd
+from pathlib import Path
+
+
+def fetch_stock_data(ticker: str, start_date: str, end_date: str) -> pd.DataFrame:
+    """
+    Fetch historical stock price data from Yahoo Finance.
+    """
+    df = yf.download(ticker, start=start_date, end=end_date)
+    return df
+
+
+def save_raw_data(df: pd.DataFrame, ticker: str):
+    """
+    Save raw stock data to data/raw directory.
+    """
+    data_dir = Path("data/raw")
+    data_dir.mkdir(parents=True, exist_ok=True)
+
+    file_path = data_dir / f"{ticker}.csv"
+    df.to_csv(file_path)
+    print(f"Raw data saved to {file_path}")
+
+
+if __name__ == "__main__":
+    ticker = "AAPL"          # we start with ONE stock
+    start_date = "2023-01-01"
+    end_date = "2025-01-01"
+
+    df = fetch_stock_data(ticker, start_date, end_date)
+    save_raw_data(df, ticker)
